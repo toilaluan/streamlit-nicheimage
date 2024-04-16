ce13f5fc8d9bdfdc26bbf74b19cff36df6ab2a6b
toilaluan <toilaluan@github.com>
2024-04-16 06:57:13 +0000

    update

---
+++ pages/0_📂_NicheDataset.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from pymongo import MongoClient
+import pandas as pd
+
+MONGO_DB_HOST = st.secrets["MONGO_DB_HOST"]
+MONGO_DB_PORT = st.secrets["MONGO_DB_PORT"]
+MONGO_DB_USER = st.secrets["MONGO_DB_USER"]
+MONGO_DB_PASS = st.secrets["MONGO_DB_PASS"]
+
+mongo_client = MongoClient(
+    "mongodb://%s:%s@%s:%s"
+    % (MONGO_DB_USER, MONGO_DB_PASS, MONGO_DB_HOST, MONGO_DB_PORT)
+)
+db = mongo_client["nicheimage"]
+image_collection = db["images"]
+text_collection = db["texts"]
+
+tabs = st.tabs(["🌆 NicheImage", "📚 NicheText"])
+with tabs[0]:
+    st.title("🌆 NicheImage Database")
+    model_name = st.selectbox(
+        "Select Model", ["GoJourney", "AnimeV3", "RealitiesEdgeXL", "RealisticVision"]
+    )
+    query = {"model_name": model_name}
+
+    cursor = image_collection.find(query, limit=32)
+    cursor.sort("_id", -1)
+    n_row = 2
+    count = 0
+    cols = st.columns(n_row)
+    for image in cursor:
+        bucket_name = image["bucket"]
+        object_key = image["key"]
+        image_url = f"http://{bucket_name}.s3.amazonaws.com/{object_key}"
+        cols[count % n_row].image(
+            image_url, caption=image["prompt"], use_column_width=True
+        )
+        count += 1
+
+with tabs[1]:
+    st.title("📚 NicheText Database")
+    model_name = st.selectbox("Select Model", ["Gemma7b"])
+    query = {"metadata.model_name": model_name}
+
+    cursor = text_collection.find(query, limit=48)
+    data = []
+    for text in cursor:
+        prompt_input = text["prompt_input"]
+        output = text["prompt_output"]["choices"][0]["text"]
+        data.append({"prompt_input": prompt_input, "prompt_output": output})
+    df = pd.DataFrame(data)
+    st.table(df)
--- pages/1_🔆_StickerMaker.py
+++ pages/1_🔆_StickerMaker.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 import base64
 import io
 import random
