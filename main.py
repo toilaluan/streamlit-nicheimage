99cfbaafe0e63c466fe8bd3d30d35ee80f2d1725
Tran Thanh Luan <92072154+toilaluan@users.noreply.github.com>
2024-08-27 14:14:12 +0700

    update

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+st.markdown(
+    """
+    <div align="center">
+[![Discord Chat](https://img.shields.io/badge/bittensor-discord-green?logo=discord)](https://discord.com/channels/799672011265015819/1191833510021955695)
+[![Github](https://img.shields.io/badge/nicheimage-github-blue?logo=github)](https://github.com/SocialTensor/SocialTensorSubnet) 
+</div>
+    """,
+    unsafe_allow_html=True
+)
 _, col1, col2, _ = st.columns([2,1,1,2])
 with col1:
     st.page_link("https://github.com/SocialTensor/SocialTensorSubnet", label="Github", icon=":material/code:")
